480ed799f815171ebda8282711aed46a6ab3b397
nadine-stritzelberger <nadine.stritzelberger@gmail.com>
2022-03-15 15:38:21 +0000

    Another change

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -24,6 +24,3 @@ def english_to_french(english_text):
 def french_to_english(french_text):
     english_text = translator.translate(text=french_text, model_id='fr-en').get_result()['translations'][0]['translation']
     return english_text
-
-print(french_to_english('Bonjour')
-print(french_to_english(' '))
